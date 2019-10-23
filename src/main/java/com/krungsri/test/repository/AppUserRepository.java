c3a4beb60cf83bb08d6dbd140c5db54c3ab9c7e9
Panupong Sanprasit <sanprasit.panupong@gmail.com>
2019-10-23 14:16:07 +0700

    Update AppUserRepository.java

--- src/main/java/com/krungsri/test/repository/AppUserRepository.java
+++ src/main/java/com/krungsri/test/repository/AppUserRepository.java
@@ -3,9 +3,6 @@ package com.krungsri.test.repository;
 import com.krungsri.test.entity.AppUser;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by nydiarra on 06/05/17.
- */
 public interface AppUserRepository extends CrudRepository<AppUser, Long> {
     AppUser findByUsername(String username);
 }
