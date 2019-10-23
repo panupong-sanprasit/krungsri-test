1ab4ec1185b0bdec4ac514bd811406b827a639fa
Panupong Sanprasit <sanprasit.panupong@gmail.com>
2019-10-23 14:15:41 +0700

    Update AppRoleRepository.java

--- src/main/java/com/krungsri/test/repository/AppRoleRepository.java
+++ src/main/java/com/krungsri/test/repository/AppRoleRepository.java
@@ -3,8 +3,5 @@ package com.krungsri.test.repository;
 import com.krungsri.test.entity.AppRole;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by nydiarra on 06/05/17.
- */
 public interface AppRoleRepository extends CrudRepository<AppRole, Long> {
 }
