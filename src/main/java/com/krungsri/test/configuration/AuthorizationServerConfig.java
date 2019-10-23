110cac96a6122d9ac27873f2d2a08076f8ab5bb7
Panupong Sanprasit <sanprasit.panupong@gmail.com>
2019-10-23 14:17:56 +0700

    Update AuthorizationServerConfig.java

--- src/main/java/com/krungsri/test/configuration/AuthorizationServerConfig.java
+++ src/main/java/com/krungsri/test/configuration/AuthorizationServerConfig.java
@@ -15,9 +15,7 @@ import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
-/**
- * Created by nydiarra on 06/05/17.
- */
+
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
