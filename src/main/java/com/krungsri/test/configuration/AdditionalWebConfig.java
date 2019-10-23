e53ee3afb173245fd4c7e12ec06909c1514ff956
Panupong Sanprasit <sanprasit.panupong@gmail.com>
2019-10-23 14:18:15 +0700

    Update AdditionalWebConfig.java

--- src/main/java/com/krungsri/test/configuration/AdditionalWebConfig.java
+++ src/main/java/com/krungsri/test/configuration/AdditionalWebConfig.java
@@ -7,17 +7,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-/**
- * Created by nydiarra on 06/05/17.
- */
 @Configuration
 public class AdditionalWebConfig {
-    /**
-     * Allowing all origins, headers and methods here is only intended to keep this example simple.
-     * This is not a default recommended configuration. Make adjustments as
-     * necessary to your use case.
-     *
-     */
+   
     @Bean
     public FilterRegistrationBean corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
